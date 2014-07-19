8c68701aeee8acae5e6d7b417cf791af65cb11d0
Paul Alexander <paul@xheo.com>
2014-07-19 01:46:17 -0700

    Bump to 2.0 for gem

--- lib/shog/version.rb
+++ lib/shog/version.rb
@@ -1,5 +1,5 @@
 module Shog
-  VERSION_NUMBER  = "0.0.1"
-  VERSION_SUFFIX  = "pre"
+  VERSION_NUMBER  = "0.0.2"
+  VERSION_SUFFIX  = ""
   VERSION         = "#{VERSION_NUMBER}#{VERSION_SUFFIX}"
 end
