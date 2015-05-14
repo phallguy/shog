8d56beb7261d66274716099c5b915bf4f36dfd3f
Paul Alexander <paul@phallguy.com>
2015-05-13 20:54:56 -0700

    Fix #5 clobbering shog configuration

--- lib/shog.rb
+++ lib/shog.rb
@@ -9,6 +9,10 @@ module Shog
   # Set up formatting options for the default rails logger.
   # @see Shog::Formatter#configure
   def self.configure(&block)
-    ::Rails.logger.formatter = Shog::Formatter.new.configure &block
+    formatter = ::Rails.logger.formatter
+    unless formatter.is_a? Shog::Formatter
+      formatter = ::Rails.logger.formatter = Shog::Formatter.new
+    end
+    formatter.configure &block
   end
 end
